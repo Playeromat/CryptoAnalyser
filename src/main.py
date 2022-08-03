d55aaad61bc61e8d61a67c6ea1b151f30a69e7c1
Playeromat <benediktgensler@gmail.com>
2022-08-03 21:29:08 +0200

    power bi week analysis added

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from controller.analyser.MonthAnalyser import MonthAnalyser
 
 
 analyser = Analyser()
-# week_analyser = WeekAnalyser()
-# month_analyser = MonthAnalyser()
+week_analyser = WeekAnalyser()
+month_analyser = MonthAnalyser()
 
 analyser.run()
-# week_analyser.run()
-# month_analyser.run()
+week_analyser.run()
+month_analyser.run()
 
 
